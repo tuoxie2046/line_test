42ec58c36f4a041962acacaca0816dfa33910843
tuoxie2046 <jinze.yu.kp@gmail.com>
2016-08-25 02:51:21 +0900

    tst

--- config/initializers/processor.rb
+++ config/initializers/processor.rb
@@ -43,7 +43,6 @@ module Line
 
       # private
       def initial_processor
-        message
         #user = User.where(mid: from_mid).first_or_initialize
         #user.stage = 0
         #user.save!
@@ -51,111 +50,116 @@ module Line
 
         stage = user.stage
 
-        case stage
-        when 0
-          #regions = ["練馬", "板橋", "北", "足立", "葛飾", "杉並", "中野", "豊島", "文京", "荒川", "世田谷", "渋谷", "新宿", "千代田", "台東", "墨田", "目黒", "港", "中央", "江東", "江戸川", "品川", "大田"]
-          regions = Region.all;
-          regions.each do |region|
-            if region.name === data.content[:text]
-              user.region = region
+        if stage < 5
+          message = "==========================\n"
+          case stage
+          when 0
+            #regions = ["練馬", "板橋", "北", "足立", "葛飾", "杉並", "中野", "豊島", "文京", "荒川", "世田谷", "渋谷", "新宿", "千代田", "台東", "墨田", "目黒", "港", "中央", "江東", "江戸川", "品川", "大田"]
+            regions = Region.all;
+            regions.each do |region|
+              if region.name === data.content[:text]
+                user.region = region
+                stage += 1
+                user.stage = stage
+                user.save!
+                break
+              end
+            end
+            # regions.each do |region|
+            #   if region === data.content[:text]
+            #     data = User.where(mid: from_mid).first
+            #     #data.first.update_attributes()
+            #     stage += 1
+            #     user.stage = stage
+            #     user.save!
+            #     break
+            #   end
+            # end
+          when 1
+            case data.content[:text]
+            when "1"
               stage += 1
               user.stage = stage
               user.save!
-              break
+            when "2"
+              stage += 1
+              user.stage = stage
+              user.save!
+            when "3"
+              stage += 1
+              user.stage = stage
+              user.save!
+            when "4"
+              stage += 1
+              user.stage = stage
+              user.save!
+            else
+              message += "==========================\n"
+              message += "選択は正しくない,もう一回しましょう\n"
             end
-          end
-          # regions.each do |region|
-          #   if region === data.content[:text]
-          #     data = User.where(mid: from_mid).first
-          #     #data.first.update_attributes()
-          #     stage += 1
-          #     user.stage = stage
-          #     user.save!
-          #     break
-          #   end
-          # end
-        when 1
-          case data.content[:text]
-          when "1"
+          when 2
+            if data.content[:text].length < 10
+              message += "==========================\n"
+              message += "みじかい！！（<10文字）\n"
+            elsif data.content[:text].length < 20
+              message += "==========================\n"
+              message += "もっと詳しく教えてちょうだい　（＜20文字）\n"
+            else
+              message += "==========================\n"
+              message += "いい感じの出会いしてるじゃないの（＞20文字）\n"
+            end
             stage += 1
             user.stage = stage
             user.save!
-          when "2"
+          when 3
+            if data.content[:text].length < 20
+              message += "==========================\n"
+              message += "みじかい！もっとあるでしょう！！恥ずかしがらずに！（＜20文字）\n"
+            else
+              message += "==========================\n"
+              message += "ふぅ～～ん，素敵じゃない（＞20文字）\n"
+            end
             stage += 1
             user.stage = stage
             user.save!
-          when "3"
+          when 4
             stage += 1
             user.stage = stage
             user.save!
-          when "4"
-            stage += 1
-            user.stage = stage
-            user.save!
-          else
-            message += "==========================\n"
-            message += "選択は正しくない,もう一回しましょう\n"
           end
-        when 2
-          if data.content[:text].length < 10
-            message += "==========================\n"
-            message += "みじかい！！（<10文字）\n"
-          elsif data.content[:text].length < 20
-            message += "==========================\n"
-            message += "もっと詳しく教えてちょうだい　（＜20文字）\n"
-          else
-            message += "==========================\n"
-            message += "いい感じの出会いしてるじゃないの（＞20文字）\n"
-          end
-          stage += 1
-          user.stage = stage
-          user.save!
-        when 3
-          if data.content[:text].length < 20
-            message += "==========================\n"
-            message += "みじかい！もっとあるでしょう！！恥ずかしがらずに！（＜20文字）\n"
-          else
-            message += "==========================\n"
-            message += "ふぅ～～ん，素敵じゃない（＞20文字）\n"
-          end
-          stage += 1
-          user.stage = stage
-          user.save!
-        when 4
-          stage += 1
-          user.stage = stage
-          user.save!
-        end
 
-        case stage
-        when 0
-          message += "==========================\n"
-          message += "あなたをグループの一員として認めます！\n"
-          message += "ようこそ，悩める彼氏のための相談BOTへ"
-          message += "まずあなたが本当に悩める男なのか，私が見定めてあげるわ♪\n"
-          message += "==========================\n"
-          message += "まずあなたの地域はどこ？\n"
-          message += "以下の選択の中で１つ入力ください\n"
-          message += "練馬, 板橋, 北, 足立, 葛飾, 杉並, 中野, 豊島, 文京, 荒川, 世田谷, 渋谷, 新宿, 千代田, 台東, 墨田, 目黒, 港, 中央, 江東, 江戸川, 品川, 大田\n"
-        when 1
-          message += "==========================\n"
-          message += "今の彼女とは付き合い始めて何か月？"
-          message += "以下の選択の中の数字を入力ください\n"
-          message += "1. あら，結構長いじゃない (>12か月）\n"
-          message += "2. 彼氏歴もそこそこね， （<12か月）\n"
-          message += "3. 新米彼氏さんなのね， （<3か月）\n"
-          message += "4. 恥ずかしがらないでちゃんと答えなさ～い　(数字じゃない)\n"
-        when 2
-          message += "==========================\n"
-          message += "どこで出会ったのかしら？なれそめを詳しく教えて？\n"
-        when 3
-          message += "==========================\n"
-          message += "彼女のどこが好きなのかしら，詳しく聞かせて？\n"
-        when 4
-          message += "==========================\n"
-          message += "審査中（スタンプとかあるとかわいいなぁ）\n"
-          message += "あなたは悩める彼氏，．．．．　のようね！（全部ok）\n"
-        end 
+          case stage
+          when 0
+            message += "==========================\n"
+            message += "あなたをグループの一員として認めます！\n"
+            message += "ようこそ，悩める彼氏のための相談BOTへ"
+            message += "まずあなたが本当に悩める男なのか，私が見定めてあげるわ♪\n"
+            message += "==========================\n"
+            message += "まずあなたの地域はどこ？\n"
+            message += "以下の選択の中で１つ入力ください\n"
+            message += "練馬, 板橋, 北, 足立, 葛飾, 杉並, 中野, 豊島, 文京, 荒川, 世田谷, 渋谷, 新宿, 千代田, 台東, 墨田, 目黒, 港, 中央, 江東, 江戸川, 品川, 大田\n"
+          when 1
+            message += "==========================\n"
+            message += "今の彼女とは付き合い始めて何か月？"
+            message += "以下の選択の中の数字を入力ください\n"
+            message += "1. あら，結構長いじゃない (>12か月）\n"
+            message += "2. 彼氏歴もそこそこね， （<12か月）\n"
+            message += "3. 新米彼氏さんなのね， （<3か月）\n"
+            message += "4. 恥ずかしがらないでちゃんと答えなさ～い　(数字じゃない)\n"
+          when 2
+            message += "==========================\n"
+            message += "どこで出会ったのかしら？なれそめを詳しく教えて？\n"
+          when 3
+            message += "==========================\n"
+            message += "彼女のどこが好きなのかしら，詳しく聞かせて？\n"
+          when 4
+            message += "==========================\n"
+            message += "審査中（スタンプとかあるとかわいいなぁ）\n"
+            message += "あなたは悩める彼氏，．．．．　のようね！（全部ok）\n"
+          end
+      else 
+         message = data.content[:text]
+      end
 
         message
       end
